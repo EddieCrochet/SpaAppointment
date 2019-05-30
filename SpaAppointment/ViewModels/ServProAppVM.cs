e572b969f0cb37ca1b3bd0982d3a91498c7be4a1
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-29 19:09:37 -0500

    added my viewmodel

---
+++ SpaAppointment/ViewModels/ServProAppVM.cs
@@ -0,0 +1,14 @@
+﻿using SpaAppointment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaAppointment.ViewModels
+{
+    public class ServProAppVM
+    {
+        public Appointment appointment { get; set; }
+        public ServiceProvider serviceProvider { get; set; }
+    }
+}
